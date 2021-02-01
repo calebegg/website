c187d4745dad7742a10befe41e7d83aa9c4c06e5
Caleb <calebegg@google.com>
2021-02-01 16:19:55 -0500

    Lighthouse tweaks

--- src/cmyk.ts
+++ src/cmyk.ts
@@ -6,6 +6,7 @@ export function main(target: HTMLElement) {
   const style = document.createElement("style");
   style.textContent = css;
   target.appendChild(style);
+  target.style.background = "white";
   for (const color of "CMYK".split("")) {
     const div = document.createElement("div");
     div.className = `color-dot ${color}`;
